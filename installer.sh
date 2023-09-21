5cdf22f0ca5859f658211d2d6c96e1e624f6ed40
Foxy <firebot1020@gmail.com>
2023-09-21 21:42:56 +0330

    Fix Repository

--- installer.sh
+++ installer.sh
@@ -1,11 +1,14 @@
 #!/bin/ash
 
 ### Add Repositories & Update ###
-sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
+wget https://raw.githubusercontent.com/PacketCipher/opkg-repo/main/keys/pcipher-repo.pub
+opkg-key add pcipher-repo.pub
+
 echo "src/gz custom_arch https://raw.githubusercontent.com/PacketCipher/opkg-repo/main/$(grep "OPENWRT_ARCH" /etc/os-release | awk -F '"' '{print $2}')" >> /etc/opkg/customfeeds.conf
-# Unbloated Packages
 echo "src/gz custom_minimal https://raw.githubusercontent.com/PacketCipher/opkg-repo/main/minimal" >> /etc/opkg/customfeeds.conf
+
 opkg update
+
 
 # Install Dnsmasq-Full
 opkg remove dnsmasq && opkg install dnsmasq-full
